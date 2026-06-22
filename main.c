11fb3a1cf70e0157c97aac504781940c02563cfd
dokcn <peser@live.com>
2026-06-22 18:44:53 +0800

    led

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "gd32c2x1.h"
+#include "systick.h"
 
 static void delay(volatile uint32_t count)
 {
@@ -9,12 +10,59 @@ static void delay(volatile uint32_t count)
 
 int main(void)
 {
+    systick_config();
+
+    /* enable the LEDs GPIO clock */
+    rcu_periph_clock_enable(RCU_GPIOA);
     rcu_periph_clock_enable(RCU_GPIOD);
+    rcu_periph_clock_enable(RCU_GPIOB);
+
+    /* configure LEDs GPIO pin */
+    gpio_mode_set(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_15);
+    gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_LEVEL_1, GPIO_PIN_15);
+
+    gpio_mode_set(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_1);
+    gpio_output_options_set(GPIOD, GPIO_OTYPE_PP, GPIO_OSPEED_LEVEL_1, GPIO_PIN_1);
 
     gpio_mode_set(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_3);
     gpio_output_options_set(GPIOD, GPIO_OTYPE_PP, GPIO_OSPEED_LEVEL_1, GPIO_PIN_3);
 
-    gpio_bit_set(GPIOD, GPIO_PIN_3);  /* 常亮 */
+    gpio_mode_set(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_4);
+    gpio_output_options_set(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_LEVEL_1, GPIO_PIN_4);
 
-    while (1) {}
+    /* reset LEDs GPIO pin */
+    gpio_bit_reset(GPIOA, GPIO_PIN_15);
+    gpio_bit_reset(GPIOD, GPIO_PIN_1);
+    gpio_bit_reset(GPIOD, GPIO_PIN_3);
+    gpio_bit_reset(GPIOB, GPIO_PIN_4);
+
+    while(1) {
+        /* turn on LED1, turn off other LED */
+        gpio_bit_set(GPIOA, GPIO_PIN_15);
+        gpio_bit_reset(GPIOD, GPIO_PIN_1);
+        gpio_bit_reset(GPIOD, GPIO_PIN_3);
+        gpio_bit_reset(GPIOB, GPIO_PIN_4);
+        delay_1ms(500);
+
+        /* turn on LED2, turn off other LED  */
+        gpio_bit_reset(GPIOA, GPIO_PIN_15);
+        gpio_bit_set(GPIOD, GPIO_PIN_1);
+        gpio_bit_reset(GPIOD, GPIO_PIN_3);
+        gpio_bit_reset(GPIOB, GPIO_PIN_4);
+        delay_1ms(500);
+
+        /* turn on LED3, turn off other LED  */
+        gpio_bit_reset(GPIOA, GPIO_PIN_15);
+        gpio_bit_reset(GPIOD, GPIO_PIN_1);
+        gpio_bit_set(GPIOD, GPIO_PIN_3);
+        gpio_bit_reset(GPIOB, GPIO_PIN_4);
+        delay_1ms(500);
+
+        /* turn on LED4, turn off other LED  */
+        gpio_bit_reset(GPIOA, GPIO_PIN_15);
+        gpio_bit_reset(GPIOD, GPIO_PIN_1);
+        gpio_bit_reset(GPIOD, GPIO_PIN_3);
+        gpio_bit_set(GPIOB, GPIO_PIN_4);
+        delay_1ms(500);
+    }
 }
